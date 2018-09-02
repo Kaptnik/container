c1085ee824affefbd82f3615c6244a9e9b6811e1
Eugene Sadovoi <evgeni@eniks.com>
2018-09-02 00:23:15 -0400

    Minor refactoring

---
+++ src/Strategies/Build/PolicyRetrievingStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Unity.Builder;
+using Unity.Policy;
+
+namespace Unity.Strategies.Build
+{
+    /// <summary>
+    /// Extension methods on <see cref="IPolicyList"/> to provide convenience
+    /// overloads (generic versions, mostly).
+    /// </summary>
+    public static class PolicyRetrievingStrategy
+    {
+        public static TPolicyInterface GetPolicy<TPolicyInterface>(this IPolicyList list, INamedType buildKey)
+        {
+            return (TPolicyInterface)(buildKey == null ? null : list.Get(buildKey.Type, buildKey.Name, typeof(TPolicyInterface)) ?? 
+                                     (buildKey.Type.GetTypeInfo().IsGenericType 
+                                         ? list.Get(buildKey.Type.GetGenericTypeDefinition(), buildKey.Name, typeof(TPolicyInterface)) ?? 
+                                           list.Get(null, null, typeof(TPolicyInterface))
+                                         : list.Get(null, null, typeof(TPolicyInterface))));
+        }
+    }
+}
