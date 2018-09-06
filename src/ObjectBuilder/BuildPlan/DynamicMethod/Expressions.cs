a95ef850e600043e9c1a327066788ebebb9bd3d9
Eugene Sadovoi <evgeni@eniks.com>
2018-09-06 14:37:24 -0400

    Update Expressions.cs

--- src/ObjectBuilder/BuildPlan/DynamicMethod/Expressions.cs
+++ src/ObjectBuilder/BuildPlan/DynamicMethod/Expressions.cs
@@ -6,12 +6,31 @@ namespace Unity.ObjectBuilder.BuildPlan.DynamicMethod
 {
     public class Expressions
     {
+        #region MemberInfos
+
+        private static readonly ConstructorInfo InvalidOperationExceptionCtor =
+            typeof(InvalidOperationException).GetTypeInfo()
+                                             .DeclaredConstructors
+                                             .First(c =>
+                                             {
+                                                 var parameters = c.GetParameters();
+                                                 return 2 == parameters.Length &&
+                                                        typeof(string) == parameters[0].ParameterType &&
+                                                        typeof(Exception) == parameters[1].ParameterType;
+                                             });
+
+        private static readonly MethodInfo StringFormat = typeof(string).GetTypeInfo()
+                                                                        .DeclaredMethods
+                                                                        .First(m => m.Name == nameof(string.Format));
+        #endregion
+
+
         #region BuildContext
 
         public static readonly ParameterExpression ContextParameter = Expression.Parameter(typeof(IBuilderContext), "context");
 
-        public static readonly Expression TypeBeingConstructedProperty = Expression.Property(ContextParameter,
-            typeof(IBuilderContext).GetTypeInfo().GetDeclaredProperty(nameof(IBuilderContext.TypeBeingConstructed)));
+        //public static readonly Expression TypeBeingConstructedProperty = Expression.Property(ContextParameter,
+        //    typeof(IBuilderContext).GetTypeInfo().GetDeclaredProperty(nameof(IBuilderContext.TypeBeingConstructed)));
 
         public static readonly Expression CurrentOperationProperty = Expression.Property(ContextParameter,
             typeof(IBuilderContext).GetTypeInfo().GetDeclaredProperty(nameof(IBuilderContext.CurrentOperation)));
@@ -22,8 +41,23 @@ namespace Unity.ObjectBuilder.BuildPlan.DynamicMethod
         public static readonly Expression ClearCurrentOperationExpression =
             Expression.Assign(CurrentOperationProperty, Expression.Constant(null));
 
+        public static readonly Expression ContextBuildKeyProperty = Expression.Property(ContextParameter,
+                    typeof(IBuilderContext).GetTypeInfo().GetDeclaredProperty(nameof(IBuilderContext.BuildKey)));
+
+        public static readonly Expression ContextBuildKeyTypeProperty = Expression.Property(ContextBuildKeyProperty,
+            typeof(INamedType).GetTypeInfo().GetDeclaredProperty(nameof(INamedType.Type)));
+
         #endregion
 
+
+        #region Exceptions
+
+        public static readonly Expression ThrowCannotConstructInterface 
+            = Expression.Throw(Expression.New(InvalidOperationExceptionCtor,
+                                              Expression.Call(StringFormat, Expression.Constant(Constants.CannotConstructInterface),
+                                              ContextBuildKeyTypeProperty),
+                                              Expression.New(typeof(InvalidRegistrationException))));
         
+        #endregion
     }
 }
