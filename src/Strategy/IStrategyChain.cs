3084c0c1ecce961b84c918d07cbb31a5b168a706
Eugene Sadovoi <evgeni@eniks.com>
2018-09-15 19:45:11 -0400

    Require call by reference and apply constrain

---
+++ src/Strategy/IStrategyChain.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Collections.Generic;
+using Unity.Builder.Strategy;
+using Unity.Policy;
+
+namespace Unity.Strategy
+{
+    /// <summary>
+    /// Represents a chain of responsibility for builder strategies.
+    /// </summary>
+    public interface IStrategyChain : IEnumerable<BuilderStrategy>, IBuildPlanPolicy
+    {
+    }
+}
